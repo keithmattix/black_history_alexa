9140b5067f511cb504688ee1554b9689b2588f24
Keith Mattix <keithmattix2@gmail.com>
2016-12-30 02:49:36 -0600

    Fix access error

--- web/controllers/alexa_controller.ex
+++ web/controllers/alexa_controller.ex
@@ -18,7 +18,8 @@ defmodule BlackHistoryAlexa.AlexaController do
 
   def intent_request(conn, "GetEvent", request) do
     date =
-      request["request"]["intent"]["slots"]["Date"]["value"]
+      request
+      get_in(["request", "intent", "slots", "Date", "value"])
       |> Timex.parse("{YYYY}-{0M}-{D}")
       |> Timex.to_datetime
     month = Map.get(date, :month)
