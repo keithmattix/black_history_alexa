f94b42e9a2a7032462eefb03e309327e54f6da1d
Keith Mattix <keithmattix2@gmail.com>
2016-12-30 02:55:38 -0600

    Attempt once again to get request access right

--- web/controllers/alexa_controller.ex
+++ web/controllers/alexa_controller.ex
@@ -18,8 +18,7 @@ defmodule BlackHistoryAlexa.AlexaController do
 
   def intent_request(conn, "GetEvent", request) do
     date =
-      request
-      |> get_in(["request", "intent", "slots", "Date", "value"])
+      request.request.intent.slots["Date"]["value"]
       |> Timex.parse("{YYYY}-{0M}-{D}")
       |> Timex.to_datetime
     month = Map.get(date, :month)
