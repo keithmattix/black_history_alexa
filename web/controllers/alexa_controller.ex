9d4545c3282524888b61b4435deed0f54de70395
Keith Mattix <keithmattix2@gmail.com>
2016-12-31 03:48:31 -0600

    Add error and help text

--- web/controllers/alexa_controller.ex
+++ web/controllers/alexa_controller.ex
@@ -17,25 +17,66 @@ defmodule BlackHistoryAlexa.AlexaController do
   end
 
   def intent_request(conn, "GetEvent", request) do
-    date =
-      request.request.intent.slots["Date"]["value"]
-      |> Timex.parse!("{YYYY}-{0M}-{D}")
-      |> Timex.to_datetime
-    month = Map.get(date, :month) |> Timex.month_name |> String.downcase
-    day = Map.get(date, :day)
-    data_url = Application.get_env(:black_history_alexa, :config)[:data_url]
-    body =
-      data_url
-      |> HTTPoison.get!
-      |> Map.get(:body)
-      |> Poison.decode!
-      |> Map.get(month)
-    IO.inspect body
-    event = Enum.at(body, day)
+    case request.request.intent.slots["Date"]["value"] |> Timex.parse("{YYYY}-{0M}-{D}") do
+      {:error, _} ->
+        error_message = "I'm sorry, that was not a valid date. If you need help, just say help."
+        response =
+          %Response{}
+          |> set_output_speech(%TextOutputSpeech{text: error_message})
+
+        conn |> set_response(response)
+      {:ok, d} ->
+        date = Timex.to_datetime(d)
+        month = Map.get(date, :month) |> Timex.month_name |> String.downcase
+        day = Map.get(date, :day)
+        data_url = Application.get_env(:black_history_alexa, :config)[:data_url]
+        body =
+          data_url
+          |> HTTPoison.get!
+          |> Map.get(:body)
+          |> Poison.decode!
+          |> Map.get(month)
+        IO.inspect body
+        event = Enum.at(body, day)
+        response =
+          %Response{}
+          |> set_output_speech(%TextOutputSpeech{text: event})
+          |> set_should_end_session(true)
+        conn |> set_response(response)
+    end
+  end
+
+  def intent_request(conn, "AMAZON.HelpIntent", _request) do
+    help_ssml = """
+    <speak>
+    To find out about events in black history, just ask: What happened today?
+    <break strength="strong" /> or What happened tomorrow?
+    For any other day, for example <say-as interpret-as="date" format="md">January 2nd</say-as>,
+    ask me: What happened on <say-as interpret-as="date" format="md">January 2nd</say-as>?
+    </speak>
+    """
     response =
       %Response{}
-      |> set_output_speech(%TextOutputSpeech{text: event})
+      |> set_output_speech(%SsmlOutputSpeech{ssml: help_ssml})
+
+    conn |> set_response(response)
+  end
+
+  def intent_request(conn, intent, _request) when intent in ["AMAZON.StopIntent", "AMAZON.CancelIntent"] do
+    response =
+      %Response{}
+      |> set_output_speech(%TextOutputSpeech{text: "Goodbye."})
       |> set_should_end_session(true)
+
+    conn |> set_response(response)
+  end
+
+  def intent_request(conn, _intent, _request) do
+    error_message = "I'm sorry, that was not a valid request. If you need help, just say help."
+    response =
+      %Response{}
+      |> set_output_speech(%TextOutputSpeech{text: error_message})
+
     conn |> set_response(response)
   end
 end
