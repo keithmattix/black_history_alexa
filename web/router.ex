c1a3907869d580a5fabfe4d4a0013171264d7b28
Keith Mattix <keithmattix2@gmail.com>
2016-12-31 15:51:30 -0600

    Comment verification plug for testing

--- web/router.ex
+++ web/router.ex
@@ -4,7 +4,7 @@ defmodule BlackHistoryAlexa.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
-    plug ValidateApplicationId, "amzn1.ask.skill.c2c4f20f-f098-4699-ac56-cebccbabf66b"
+    # plug ValidateApplicationId, "amzn1.ask.skill.c2c4f20f-f098-4699-ac56-cebccbabf66b"
   end
 
   scope "/api", BlackHistoryAlexa do
